79ff1ef213106a6216315e9d6b72108d68bb5fe8
Ngalemo Patrick <ngalemopatrick@outlock.com>
2023-10-05 11:53:46 -0500

    added some changes to the header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import { Routes, Route} from 'react-router-dom'
-import About from './pages/About';
-import Home from './pages/Home';
-import NavBar from './components/NavBar';
-
-
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
+// import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header>
-        <Link  className='site-logo' >#VanLife</Link>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
         <nav>
-          <Link to='/about' >About</Link>
+          <Link to="/about">About</Link>
         </nav>
       </header>
       <Routes>
-        <Route  path='/about' element={< About />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
